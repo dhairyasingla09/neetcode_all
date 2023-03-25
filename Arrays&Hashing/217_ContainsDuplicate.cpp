99d62b02c115599b477ed142cec73088bf09cbf9
Dhairya Singla <97469815+dhairyasingla09@users.noreply.github.com>
2023-03-25 18:34:55 +0530

    Update 217_ContainsDuplicate.cpp

--- Arrays&Hashing/217_ContainsDuplicate.cpp
+++ Arrays&Hashing/217_ContainsDuplicate.cpp
@@ -1,2 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) {
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
